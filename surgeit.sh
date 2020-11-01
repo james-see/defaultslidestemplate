091181bd2dc4c1ed2efc745407c665fc2f51b36f
jamesacampbell <james@jamescampbell.us>
2020-11-01 17:38:17 -0500

    _

--- surgeit.sh
+++ surgeit.sh
@@ -1,6 +1,11 @@
 rm -rf public/*
-foldertocopy="${1:-templates}"
-cp "$foldertocopy"/index.html public/
+foldertocopy="${1:-.}"
+cp templates/"$foldertocopy"/index.html public/
+cp surge/CNAME public/
 cd public
 git yolo
 surge
+
+## example run: ./surgeit.sh cooldeck1
+
+## surge is a site to quickly deploy a static site for free
